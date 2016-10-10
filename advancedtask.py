56c3f83bedf322a71190e2da265f04bc36ee5fa5
OscarDolloway <oscar.dolloway@hotmail.co.uk>
2016-10-10 16:44:07 +0100

    Add files via upload

---
+++ advancedtask.py
@@ -0,0 +1,75 @@
+#Coursework sheet one
+
+import random
+import time
+
+
+#question1
+def shuffle():
+    alist = [1,2,3,4,5,6,7,8]
+    random.shuffle(alist)
+    print(alist)
+#shuffle()
+
+
+#question2
+
+
+def Count():
+    user = input("enter number")
+    
+
+
+##Count()
+
+
+#Advanced task
+
+
+def alien():
+
+    x = 3
+    y = 5
+    d = 30
+
+    eggs = (x*d) # there are 90eggs laid by 30 days
+    print (eggs)
+    
+alien()
+
+
+def func():
+    #3eggs are laid a day
+    #they hatch after 5 days
+    numberhatched = 3
+    numberlaid = 3
+    arr = [0]*5
+    print (arr)
+    arr [0] = 1
+    print(arr)
+    
+    
+
+#func()
+a = [1]
+hatched = [1]
+eggs = []
+print ("1 alien has arrived",hatched) 
+for i in range(1,31):
+    #i= i
+    print("day",i,time.sleep(0.1),)
+    if i == 5 or i ==10 or i ==15 or i ==20 or i ==25 or i ==30:
+        hatched.append(3)
+        eggs.append(18)
+        print("3 aliens hatched", sum(hatched),"eggs laid",sum(eggs))
+        
+  
+
+
+
+
+
+    
+
+
+
